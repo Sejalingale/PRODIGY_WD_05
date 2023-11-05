b27e724386a1d2f0cdf63f9469a50c6f529265fe
Sejalingale <sejalingale17@gmail.com>
2023-11-05 17:43:38 +0530

    done
    done

---
+++ src/apikeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "364db664039655268ec8e75f02d83fd4",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
